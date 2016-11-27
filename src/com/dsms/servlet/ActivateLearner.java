25136f31de47d9219361366e057a4b9059a391be
prakharnahar14 <prakharnahar14@gmail.com>
2016-11-27 03:34:38 -0800

    Removed unused imports

--- src/com/dsms/servlet/ActivateLearner.java
+++ src/com/dsms/servlet/ActivateLearner.java
@@ -5,17 +5,13 @@ import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
-import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
--- src/com/dsms/servlet/AdminLogin.java
+++ src/com/dsms/servlet/AdminLogin.java
@@ -14,7 +14,6 @@ import java.sql.SQLException;
 import java.util.Enumeration;
 
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
